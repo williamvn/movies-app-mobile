c3bb653a852320443d4715f0b05a757149e7d6a2
William Vazquez Noguerol <william.vazquez.noguerol@tui.be>
2023-03-30 20:43:45 +0200

    Touchable poster

---
+++ components/TouchablePoster.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { Image, StyleSheet, TouchableOpacity, View, Text } from 'react-native'
+import { Movie } from '../types/MoviesDB'
+import LinearGradient from 'react-native-linear-gradient';
+
+interface TouchablePosterProps {
+    movie: Movie
+}
+
+export const TouchablePoster = ({ movie }: TouchablePosterProps) => {
+    const uri = `https://image.tmdb.org/t/p/w500${movie?.poster_path}`;
+
+    return (
+        <View style={styles.container}>
+            <TouchableOpacity>
+                <Image source={{ uri }} style={styles.image} />
+            </TouchableOpacity>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        width: 200,
+        height: 300,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 12,
+        },
+        shadowOpacity: 0.58,
+        shadowRadius: 16.00,
+        elevation: 24,
+    },
+    image: {
+        width: 200,
+        height: 300,
+        borderRadius: 20,
+    },
+});
--- screens/MoviesScreen.tsx
+++ screens/MoviesScreen.tsx
@@ -3,6 +3,7 @@ import { ActivityIndicator, Button, Image, Text, View } from 'react-native'
 import { StackScreenProps } from '@react-navigation/stack';
 import { useMovies } from '../hooks/useMovies';
 import { TouchableHeaderPoster } from '../components/TouchableHeaderPoster';
+import { TouchablePoster } from '../components/TouchablePoster';
 
 export const MoviesScreen = (props: StackScreenProps<any>) => {
     const { movies, isLoading } = useMovies();
@@ -11,9 +12,12 @@ export const MoviesScreen = (props: StackScreenProps<any>) => {
         <ActivityIndicator color={"red"} size={60} />
         :
         (
-            <View style={{flex: 1, backgroundColor: "black"}}>
+            <View style={{ flex: 1, backgroundColor: "white" }}>
                 {/* <Image source={movies[0].poster_path} /> */}
-                <TouchableHeaderPoster movie={movies[3]} />
+                <View style={{marginBottom: 30}}>
+                    <TouchableHeaderPoster movie={movies[3]} />
+                </View>
+                <TouchablePoster movie={movies[0]} />
             </View>
         )
 }
