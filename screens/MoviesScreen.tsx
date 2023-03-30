51308e7149bb51ed9da564286748c5763f9151aa
William Vazquez Noguerol <william.vazquez.noguerol@tui.be>
2023-03-30 21:15:07 +0200

    Add Carrousel

--- screens/MoviesScreen.tsx
+++ screens/MoviesScreen.tsx
@@ -4,6 +4,7 @@ import { StackScreenProps } from '@react-navigation/stack';
 import { useMovies } from '../hooks/useMovies';
 import { TouchableHeaderPoster } from '../components/TouchableHeaderPoster';
 import { TouchablePoster } from '../components/TouchablePoster';
+import Carousel from 'react-native-snap-carousel';
 
 export const MoviesScreen = (props: StackScreenProps<any>) => {
     const { movies, isLoading } = useMovies();
@@ -12,12 +13,32 @@ export const MoviesScreen = (props: StackScreenProps<any>) => {
         <ActivityIndicator color={"red"} size={60} />
         :
         (
-            <View style={{ flex: 1, backgroundColor: "white" }}>
-                {/* <Image source={movies[0].poster_path} /> */}
-                <View style={{marginBottom: 30}}>
-                    <TouchableHeaderPoster movie={movies[3]} />
-                </View>
-                <TouchablePoster movie={movies[0]} />
+            <View style={{ flex: 1, backgroundColor: "black" }}>
+
+                <Carousel
+                    data={movies}
+                    renderItem={({ item }) => (
+                        <View style={{ marginBottom: 5 }}>
+                            <TouchableHeaderPoster movie={item} />
+                        </View>
+                    )}
+                    sliderWidth={500}
+                    itemWidth={500}
+                    autoplay
+                />
+
+                <Carousel
+                    data={movies}
+                    renderItem={({ item }) => (
+                        <View style={{ marginBottom: 30 }}>
+                            <TouchablePoster movie={item} />
+                        </View>
+                    )}
+                    sliderWidth={500}
+                    itemWidth={200}
+                    centerContent={true}
+                />
+                
             </View>
         )
 }
